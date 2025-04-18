59cd9baf1e73ca4307af4cff9520e9b8832506a7
xed <ana1lphaon@gmail.com>
2025-04-18 15:21:14 +0300

    added connection to db

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,11 +1,12 @@
 #include "crow.h"
 #include "db/Database.h"
 #include "routes/Routes.h"
+#include <cstdlib>
 
 int main()
 {
     crow::SimpleApp app;
-    Database db;
+    Database db(std::getenv("DB_CONN_STRING"));
     Routes routes(app, db);
 
     app.port(8080).multithreaded().run();
