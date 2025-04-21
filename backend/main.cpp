3aa776609d04c93638fee9d1152eb281b2894963
XxKotfeJxX <povelitel.bog.lol@gmail.com>
2025-04-21 19:08:30 +0300

    Тестовий фікс CMake кросплатформ

--- backend/main.cpp
+++ backend/main.cpp
@@ -2,13 +2,22 @@
 #include "db/Database.h"
 #include "routes/Routes.h"
 #include <cstdlib>
+#include <laserpants/dotenv/dotenv.h>
 
 int main()
 {
+    dotenv::init(); // завантажує .env
     crow::SimpleApp app;
-    const char* env = std::getenv("DB_CONN_STRING");
+
+    const char *env = std::getenv("DB_CONN_STRING");
+    if (!env)
+    {
+        std::cerr << "DB_CONN_STRING not set!" << std::endl;
+        return 1;
+    }
+
     Database db(env);
     Routes routes(app, db);
 
-    app.port(8080).multithreaded().run();
+    app.port(18080).multithreaded().run();
 }
