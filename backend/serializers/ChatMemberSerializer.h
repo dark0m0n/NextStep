50ee65b19ca37c001b08a94e59dc52d9a33764fc
xed <ana1lphaon@gmail.com>
2025-04-27 12:26:36 +0300

    created ChatMemberSerializer

---
+++ backend/serializers/ChatMemberSerializer.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include <optional>
+#include <nlohmann/json.hpp>
+#include "../models/ChatMember.h"
+
+using json = nlohmann::json;
+
+class ChatMemberSerializer {
+public:
+    static json serializeChatMember(const ChatMember &chatMember) {
+        return {
+            {"id", chatMember.getID()},
+            {"chatID", chatMember.getChatID()},
+            {"userID", chatMember.getUserID()}
+        };
+    }
+
+    static json serializeChatMembers(const std::vector<ChatMember> &chatMembers) {
+        json j;
+        for (const auto &chatMember: chatMembers) {
+            j.push_back(serializeChatMember(chatMember));
+        }
+        return j;
+    }
+
+    static json serializeOptionalChatMember(const std::optional<ChatMember> &chatMember) {
+        if (chatMember.has_value()) {
+            return serializeChatMember(chatMember.value());
+        }
+        return nullptr;
+    }
+};
