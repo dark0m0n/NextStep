bad82297e30e88b27c06a767d6cbf7ef19e161ba
xed <ana1lphaon@gmail.com>
2025-05-04 01:18:13 +0300

    created ChatMemberRoute

---
+++ backend/routes/ChatMemberRoute.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../controllers/ChatMemberController.h"
+#include "crow.h"
+
+class ChatMemberRoute {
+public:
+    static void registerRoutes(crow::SimpleApp &app, Database &db) {
+        ChatMemberController controller(db);
+
+        CROW_ROUTE(app, "/api/chat/members/<int>").methods(crow::HTTPMethod::Get)
+        ([&controller](const int chatID) {
+            return controller.getChatMember(chatID);
+        });
+
+        CROW_ROUTE(app, "/api/chat/member/<int>").methods(crow::HTTPMethod::Get)
+        ([&controller](const int id) {
+            return controller.getChatMember(id);
+        });
+
+        CROW_ROUTE(app, "/api/chat/member").methods(crow::HTTPMethod::Post)
+        ([&controller](const crow::request &req) {
+            return controller.createChatMember(const_cast<crow::request &>(req));
+        });
+    }
+};
