ab940e99e25de78ac04988346da27faa5d2dd428
xed <ana1lphaon@gmail.com>
2025-05-04 01:18:31 +0300

    added routes

--- backend/routes/Routes.h
+++ backend/routes/Routes.h
@@ -1,21 +1,21 @@
 #pragma once
 #include "crow.h"
 #include "../db/Database.h"
+#include "UserRoute.h"
+#include "StartupRoute.h"
+#include "ReviewRoute.h"
+#include "ChatRoute.h"
+#include "MessageRoute.h"
+#include "ChatMemberRoute.h"
 
-class Routes
-{
-private:
-    
+class Routes {
 public:
-    Routes(crow::SimpleApp& app, Database& db);
-    ~Routes();
+    Routes(crow::SimpleApp &app, Database &db) {
+        UserRoute::registerRoutes(app, db);
+        StartupRoute::registerRoutes(app, db);
+        ReviewRoute::registerRoutes(app, db);
+        ChatRoute::registerRoutes(app, db);
+        MessageRoute::registerRoutes(app, db);
+        ChatMemberRoute::registerRoutes(app, db);
+    }
 };
-
-Routes::Routes(crow::SimpleApp& app, Database& db)
-{   
-}
-
-Routes::~Routes()
-{
-}
-
