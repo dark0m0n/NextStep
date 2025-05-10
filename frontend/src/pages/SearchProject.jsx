751fc149f1dcc342cf74d1fbf7faef99648dcb74
Bravissimo <147399741+Achuyon@users.noreply.github.com>
2025-05-10 12:25:35 +0300

    + a href

--- frontend/src/pages/SearchProject.jsx
+++ frontend/src/pages/SearchProject.jsx
@@ -4,43 +4,44 @@ import MyFooter from "../components/Footer.jsx";
 
 export default function SearchPage() {
 
-  return (
-    <>
-      <MyHeader/>
+    return (
+        <>
+            <MyHeader />
 
-      <section className="searchproj-section">
-        <div className="info-searchproj">
-          <h2>Знайти стартап</h2>
-          <p>
-            Знайди вже готовий і правильно організований проєкт, який чекає на
-            персонал
-          </p>
-        </div>
+            <section className="searchproj-section">
+                <div className="info-searchproj">
+                    <h2>Знайти стартап</h2>
+                    <p>
+                        Знайди вже готовий і правильно організований проєкт, який чекає на
+                        персонал
+                    </p>
+                </div>
 
-        <div className="blockss-searchproj">
-          <div className="process-searchproj">
-            <p>10,000+ результатів</p>
-            <p>Фільтр</p>
-            <p>Сортувати: за ціною</p>
-          </div>
+                <div className="blockss-searchproj">
+                    <div className="process-searchproj">
+                        <p>10,000+ результатів</p>
+                        <p>Фільтр</p>
+                        <p>Сортувати: за ціною</p>
+                    </div>
 
-          <div className="blocks-searchproj">
-            {[...Array(16)].map((_, index) => (
-              <div className="block-searchproj" data-tags="IT, Технології" key={index}>
-                <img src="images/ua.png" alt="UA flag" />
-                <p className="title">
-                  Кохайтеся, чорнобриві, Та не з москалями, Бо москалі — чужі
-                  люде, Роблять лихо з вами
-                </p>
-                <p className="mark">卐 9.5</p>
-                <p className="price">Від 5000 грн</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
+                    <div className="blocks-searchproj">
+                        {[...Array(16)].map((_, index) => (
+                            <div className="block-searchproj" data-tags="IT, Технології" key={index}>
+                                <a href="/project">
+                                    <img src="images/ua.png" alt="UA flag" />
+                                    <p className="title">
+                                        Кохайтеся, чорнобриві, Та не з москалями, Бо москалі — чужі
+                                        люде, Роблять лихо з вами
+                                    </p>
+                                    <p className="mark">卐 9.5</p>
+                                    <p className="price">Від 5000 грн</p>
+                                </a></div>
+                        ))}
+                    </div>
+                </div>
+            </section>
 
-      <MyFooter/>
-    </>
-  );
+            <MyFooter />
+        </>
+    );
 }
