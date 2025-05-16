7c65b64a234c484be48fc15858db72578d98cbbe
xed <ana1lphaon@gmail.com>
2025-05-16 10:10:56 +0300

    Add CORS support to backend main.cpp

--- backend/main.cpp
+++ backend/main.cpp
@@ -2,13 +2,26 @@
 #include "db/Database.h"
 #include "routes/Routes.h"
 #include "../middleware/AuthMiddleware.h"
+#include "crow/middlewares/cors.h"
 #include <cstdlib>
 
 int main() {
-    crow::App<AuthMiddleware> app;
+    crow::App<AuthMiddleware, crow::CORSHandler> app;
+
+    auto& cors = app.get_middleware<crow::CORSHandler>();
+
+    cors
+      .global()
+        .headers("X-Custom-Header", "Upgrade-Insecure-Requests")
+        .methods("GET"_method, "POST"_method, "PUT"_method, "DELETE"_method, "OPTIONS"_method)
+      .prefix("/cors")
+        .origin("*")
+      .prefix("/nocors")
+        .ignore();
+
     const char *env = std::getenv("DB_CONN_STRING");
     Database db(env);
-    [[maybe_unused]] Routes<crow::App<AuthMiddleware>> routes(app, db);
+    [[maybe_unused]] Routes routes(app, db);
 
     app.port(8000).multithreaded().run();
 }
