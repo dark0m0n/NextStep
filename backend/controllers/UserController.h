be7fceafdeb541e86139d21233aa4cf9d2ee4f14
xed <ana1lphaon@gmail.com>
2025-05-22 20:03:09 +0300

    Refactor UserController::getMe method to include authentication check

--- backend/controllers/UserController.h
+++ backend/controllers/UserController.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "../db/Database.h"
 #include "crow.h"
+#include "../middleware/AuthMiddleware.h"
+#include "../serializers/UserSerializer.h"
 
 class UserController {
     Database &db;
@@ -19,5 +21,16 @@ public:
     [[nodiscard]] crow::response login(const crow::request &req) const;
 
     template <typename App>
-    [[nodiscard]] crow::response getMe(App &app, const crow::request &req) const;
+    [[nodiscard]] crow::response getMe(App &app, const crow::request &req) const {
+        auto &ctx = app.template get_context<AuthMiddleware>(req);
+
+        if (!ctx.auth.authorized) {
+            return crow::response{401, "Unauthorized"};
+        }
+
+        auto user = db.getUserByUsername(ctx.auth.username);
+
+        return crow::response{UserSerializer::serializeOptionalUser(user).dump()};
+    }
+
 };
